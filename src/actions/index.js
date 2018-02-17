667c6747b2a9433c44e3131611cc19e583e53c6a
Shawn Levin <shawnlevin22@gmail.com>
2018-02-16 17:56:37 -0800

    connected to server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,26 @@
 import types from './types';
+import axios from 'axios';
+
+
+const BASE_URL = 'http://api.reactprototypes.com';
+
+export function signUp(cred) {
+    return dispatch => {
+        axios.post(`${BASE_URL}/signup`, cred).then(resp => {
+            console.log('sign up response: ', resp)
+        }).catch(err => {
+            console.log('sign up error', err.message);
+        });
+    }
+}
+
+export function signIn(cred) {
+    return async dispatch => {
+        try {
+            const response = await axios.post(`${BASE_URL}/signin`, cred);
+            console.log("sign in response ", response);
+        } catch (err) {
+            console.log('sign in error', err.message);
+        }
+    }
+}
--- src/components/sign_in.js
+++ src/components/sign_in.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { renderInput } from '../helpers';
+import { connect } from 'react-redux';
+import { signIn } from '../actions';
 
 class SignIn extends Component {
     handleSignIn(values) {
         console.log('sign in submitted :', values)
+        this.props.signIn(values);
     }
 
 
@@ -17,7 +20,7 @@ class SignIn extends Component {
                     <div className="col s6 offset-s3">
                         <div className="card amber lighten-5">
                             <div className="card-content">
-                                <form onSubmit={handleSubmit(this.handleSignIn)} >
+                                <form onSubmit={handleSubmit(this.handleSignIn.bind(this))} >
                                     <Field component={renderInput} name="email" placeholder="Enter your email" />
                                     <Field component={renderInput} name="password" placeholder="Enter your password" type="password" />
                                     <Field component={renderInput} name="confirmPassword" placeholder="Confirm password" type="password" />
@@ -37,4 +40,4 @@ SignIn = reduxForm({
     form: 'sign-in-from'
 })(SignIn);
 
-export default SignIn;
+export default connect(null, { signIn })(SignIn);
