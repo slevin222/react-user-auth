0695bcea4f5dcc20d163bb3413a67778184c9982
Shawn Levin <shawnlevin22@gmail.com>
2018-02-20 13:06:02 -0800

    auth routes updated

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ import About from './about';
 import Quote from './quote';
 import SignIn from './sign_in';
 import SignUp from './sign_up';
+import auth from '../hoc/auth';
 
 
 
@@ -16,7 +17,7 @@ const App = () => (
         <Nav />
         <Route exact path="/" component={Home} />
         <Route path="/about-us" component={About} />
-        <Route path="/movie-quote" component={Quote} />
+        <Route path="/movie-quote" component={auth(Quote)} />
         <Route path="/sign-in" component={SignIn} />
         <Route path="/sign-up" component={SignUp} />
     </div>
---
+++ src/hoc/auth.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function (WrappedComponent) {
+    class Auth extends Component {
+
+        componentDidMount() {
+            console.log("is user auth correct?", this.props.auth);
+
+            if (!this.props.auth) {
+                this.props.history.push('/');
+            }
+        }
+
+        componentWillReceiveProps(nextProps) {
+            if (!nextProps.auth) {
+                this.props.history.push('/');
+            }
+        }
+
+        render() {
+            return <WrappedComponent {...this.props} />
+        }
+    }
+
+    function mapStateToProps(state) {
+        return {
+            auth: state.user.auth
+        }
+    }
+
+    return connect(mapStateToProps)(Auth);
+}
